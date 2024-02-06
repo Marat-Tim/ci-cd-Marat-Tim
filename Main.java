4308f6deee929271903091adcc2a6f1b2670df11
Marat-Tim <rurikcat@gmail.com>
2024-02-06 19:16:55 +0300

    добавил приветствие пользователя перед выводом ответа

--- Main.java
+++ Main.java
@@ -17,6 +17,7 @@ public class Main {
                 return;
             }
             int b = scanner.nextInt();
+            System.out.println("Привет мир");
             System.out.println(a + b);
         }
     }
