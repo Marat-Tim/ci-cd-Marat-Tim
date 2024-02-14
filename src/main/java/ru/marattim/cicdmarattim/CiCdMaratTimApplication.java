d422b5c4ad54e147631fa63a9dd28d533bb3340d
Marat-Tim <rurikcat@gmail.com>
2024-02-15 02:09:25 +0300

    переделал проект в spring приложение

---
+++ src/main/java/ru/marattim/cicdmarattim/CiCdMaratTimApplication.java
@@ -0,0 +1,11 @@
+package ru.marattim.cicdmarattim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdMaratTimApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdMaratTimApplication.class, args);
+    }
+}
