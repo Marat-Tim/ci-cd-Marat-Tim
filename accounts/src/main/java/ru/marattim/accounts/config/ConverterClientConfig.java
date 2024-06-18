f25dbda8737a0331f71bc3e76bfdb56792efc29a
Marat-Tim <rurikcat@gmail.com>
2024-06-18 14:40:47 +0300

    Исправил проблему, что запросы не могут выполняться без servlet контекста

--- accounts/src/main/java/ru/marattim/accounts/config/ConverterClientConfig.java
+++ accounts/src/main/java/ru/marattim/accounts/config/ConverterClientConfig.java
@@ -3,8 +3,8 @@ package ru.marattim.accounts.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,10 +12,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ConverterClientConfig {
     @Bean
     public WebClient getWebClient(@Value("${converter.url}") String converterUrl,
-                                  ClientRegistrationRepository clientRegistrationRepository,
-                                  OAuth2AuthorizedClientRepository authorizedClientRepository) {
+                                  OAuth2AuthorizedClientManager authorizedClientManager) {
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
         oauth2.setDefaultOAuth2AuthorizedClient(true);
         oauth2.setDefaultClientRegistrationId("keycloak");
 
@@ -23,6 +22,24 @@ public class ConverterClientConfig {
                 .baseUrl(converterUrl)
                 .apply(oauth2.oauth2Configuration())
                 .build();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientService clientService) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, clientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
     }
 
 }
